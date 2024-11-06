17a63138954b3d64b86e91392b0433310e5517bd
Abdulajishs <abdulajeeshsb@gmail.com>
2024-11-06 19:03:27 +0530

    checkout page and some changes

--- index.js
+++ index.js
@@ -18,3 +18,26 @@ function toggleMenu() {
         document.body.style.overflow = "auto";
     }
 }
+
+
+// document.addEventListener('DOMContentLoaded',function () {
+//     const checkbox = document.getElementById("differentBilling")
+    
+// }
+// )
+function diffBilling() {
+    const addressFields = document.getElementById('diff');
+    addressFields.classList.toggle("active")
+    // Get all elements with class 'diff'
+    // let diffElements = document.querySelectorAll('diff');
+    // let isChecked = document.getElementById('differentBilling').checked;
+
+    // Toggle the 'active' class on each element
+    // diffElements.forEach(element => {
+    //     if (isChecked) {
+    //         element.classList.add('active');
+    //     } else {
+    //         element.classList.remove('active');
+    //     }
+    // });
+}
